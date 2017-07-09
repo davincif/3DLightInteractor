fb35498f4eba9b6ce943a8461ae01a92bea254ba
davincif <LDaVinciF@gmail.com>
2017-07-09 13:41:20 -0300

    model loading bug fixed

--- model3D/model.py
+++ model3D/model.py
@@ -4,8 +4,6 @@ import conf
 class Model:
 	vertices = None #(x, y, z)
 	vtc_qtd = 0
-	edges = None #((v1), (v2))
-	edg_qtd = 0
 	surfaces = None #((v1), (v2), (v3))
 	sfc_qtd = 0
 
@@ -18,10 +16,7 @@ class Model:
 		line = co_file.readline()
 		n = line.find(" ")
 		self.vtc_qtd = int(line[:n])
-		if(conf.settings["edges"] == True):
-			self.edg_qtd = int(line[n+1:])
-		if(conf.settings["surfaces"] == True):
-			self.sfc_qtd = int(line[n+1:])
+		self.sfc_qtd = int(line[n+1:])
 
 
 		#get points
@@ -36,34 +31,22 @@ class Model:
 		self.vertices = tuple(auxl)
 		print("\t" + str(self.vtc_qtd) + " vertices loaded")
 
+
 		#get triangles
 		print("\n\tloading triangles...")
 		auxl = []
-		auxs = []
-		for count in range(0, self.edg_qtd):
+		for count in range(0, self.sfc_qtd):
 			line = co_file.readline()
 			if(conf.settings["isply"] == True):
 					line = line[line.find(" ")+1:]
 			n1 = line.find(" ")
-			x = int(line[:n1]) - 1
+			x = int(line[:n1])
 			n2 = n1 + line[n1+1:].find(" ") + 1
-			y = int(line[n1:n2]) - 1
-			z = int(line[n2+1:]) - 1
+			auxl.append( (x, int(line[n1:n2]), int(line[n2+1:])) )
 
-			if(conf.settings["edges"] == True):
-				auxl.append( (x, y) )
-				auxl.append( (y, z) )
-			
-			if(conf.settings["surfaces"] == True):
-				auxs.append( (x, y, z) )
+		self.surfaces = tuple(auxl)
 
-		if(conf.settings["edges"] == True):
-			self.edges = tuple(auxl)
-
-		if(conf.settings["surfaces"] == True):
-			self.surfaces = tuple(auxs)
-
-		print("\t" + str(self.edg_qtd) + " edges loaded")
+		print("\t" + str(self.sfc_qtd) + " surfaces loaded")
 
 		print("loaded\n")
 		co_file.close()
