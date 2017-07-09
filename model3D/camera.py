9655b78117e2a1eaf0860a53e2fd0c6a2c38bba7
davincif <LDaVinciF@gmail.com>
2017-07-09 20:19:56 -0300

    setting the camera's vectors

--- model3D/camera.py
+++ model3D/camera.py
@@ -14,6 +14,7 @@ class Camera:
 	pos = None #posotion = Point(x, y, z)
 	v_N = None #Vector N = Vector(x, y, z)
 	v_V = None #Vector V = Vector(x, y, z)
+	v_U = None #Vector orthogonal to N and V
 	d = None #d
 	h = None #(hx, hy)
 	mdl = None #the model3D to be printed
@@ -61,9 +62,26 @@ class Camera:
 		print("loaded\n")
 		cc_file.close()
 
+		#create camera canonical base
+		self.orthogonalize()
+
 	def set(self, width, height):
 		self.display = (800, 600)
 		self.screen = pygame.display.set_mode(self.display)
+
+	def orthogonalize(self):
+		###
+		# find a v_U orthogonal to v_V and v_N
+		#see in: https://www.khanacademy.org/math/linear-algebra/alternate-bases/change-of-basis/v/linear-algebra-change-of-basis-matrix
+		#and in: http://ccsubs.com/video/yt%3ArHonltF77zI/the-gram-schmidt-process-alternate-coordinate-systems-bases-linear-algebra-khan-academy/subtitles
+		###
+
+		#v1 = self.v_N
+		#v2 = self.v_V
+		#u2 = self.v_U
+		u1 = self.v_V
+		self.v_V = (self.v_V - self.v_N.projection(self.v_V)).normalize()
+		self.v_U = self.v_N.crossProd(self.v_V)
 
 	def draw(self):
 		#printing edges
@@ -71,3 +89,20 @@ class Camera:
 			for triangle in self.mdl.surfaces:
 				for vertex in triangle:
 					self.screen.set_at((int(self.mdl.vertices[vertex].x), int(self.mdl.vertices[vertex].y)), Color(255, 255, 255, 0))
+
+	#debugging methods
+	def print(self):
+		print("Camera:")
+		print("\tposition at" + str(self.pos))
+		print("\tvector N " + str(self.v_N))
+		print("\tvector V " + str(self.v_V))
+		print("\tvector U " + str(self.v_U))
+		print("\td " + str(self.d))
+		print("\th " + str(self.h))
+		print("\thas a model? " + str(self.mdl is not None))
+		if self.display is None:
+			print("\thas display? False")
+		else:
+			print("\thas display? " + str(self.display))
+		print("\thas screen? " + str(self.screen is not None))
+		print("\n")
