92c77b438b9f05abf847d118ff13c05658e6053a
davincif <LDaVinciF@gmail.com>
2017-07-09 21:35:20 -0300

    load more than one light

--- model3D/light.py
+++ model3D/light.py
@@ -1,8 +1,3 @@
-#application internal imports
-from algebra import Vector
-from algebra import Point
-import conf
-
 class Light(object):
 	lp = None	#light position Point(x, y, v)
 	ka = None	#ka - ambiental reflection
@@ -13,55 +8,15 @@ class Light(object):
 	Il = None	#Il - Light source color
 	n = None	#n  - Roughness constant
 
-	def __init__(self):
-		#load informations from file
-		cl_file = open(conf.illumination, "r")
-
-		print("from file:" + conf.illumination)
-		print("\tloaded lights settings...")
-
-		#loading light position (world coordinate)
-		line = cl_file.readline()
-		n1 = line.find(" ")
-		x = float(line[:n1])
-		n2 = n1 + line[n1+1:].find(" ") + 1
-		self.pos = Point(x, float(line[n1:n2]), float(line[n2+1:]))
-
-		#ka loading
-		self.ka = float(cl_file.readline())
-
-		#Ia loading
-		line = cl_file.readline()
-		n1 = line.find(" ")
-		x = float(line[:n1])
-		n2 = n1 + line[n1+1:].find(" ") + 1
-		self.Ia = Vector(x, float(line[n1:n2]), float(line[n2+1:]))
-
-		#kd loading
-		self.kd = float(cl_file.readline())
-
-		#Od loading
-		line = cl_file.readline()
-		n1 = line.find(" ")
-		x = float(line[:n1])
-		n2 = n1 + line[n1+1:].find(" ") + 1
-		self.Od = Vector(x, float(line[n1:n2]), float(line[n2+1:]))
-
-		#ks loading
-		self.ks = float(cl_file.readline())
-
-		#Il loading
-		line = cl_file.readline()
-		n1 = line.find(" ")
-		x = float(line[:n1])
-		n2 = n1 + line[n1+1:].find(" ") + 1
-		self.Il = Vector(x, float(line[n1:n2]), float(line[n2+1:]))
-
-		#n loading
-		self.n = float(cl_file.readline())
-
-		print("loaded\n")
-		cl_file.close()
+	def __init__(self, pos, ka, Ia, kd, Od, ks, Il, n):
+		self.lp = pos
+		self.ka = ka
+		self.Ia = Ia
+		self.kd = kd
+		self.Od = Od
+		self.ks = ks
+		self.Il = Il
+		self.n = n
 
 	#debugging methods
 	def print(self):
@@ -75,16 +30,3 @@ class Light(object):
 		print("\tLight source color: " + str(self.Il))
 		print("\tRoughness constant: " + str(self.n))
 		print("\n")
-
-# luminacao.txt
-# /---------------------------------------\
-# | -200 -50 300                          | ; Pl - Posicao da luz em coordenadas de mundo
-# | 1                                     | ; ka - reflexao ambiental
-# | 2 2 2                                 | ; Ia - vetor cor ambiental
-# | 1                                     | ; kd - constante difusa
-# | 1 1 1                                 | ; Od - vetor difuso
-# | 0.5                                   | ; ks - parte especular
-# | 0 255 0                               | ; Il - cor da fonte de luz
-# | 2                                     | ; n  - constante de rugosidade
-# |                                       |
-# \---------------------------------------/
--- world.py
+++ world.py
@@ -1,17 +1,85 @@
 #application internal imports
+from algebra import Vector
+from algebra import Point
 from model3D.model import Model
 from model3D.camera import Camera
 from model3D.light import Light
+import conf
 
 class World(object):
 	model = None # 3D model to be loaded
 	cam = None # camera (thing that sees the world)
 	lpl = [] #light points list
+	lp_qtd = 0 #light points quantity
 	win_size = None #(width, height)
 
 	def __init__(self, win_size):
 		self.win_size = win_size
 		self.model = Model()
 		self.cam = Camera(self.model)
-		self.lp = Light() #light point
+		self.lpl = self.__load_lights() #light point
+		self.lp_qtd = len(self.lpl)
 		self.cam.set(self.win_size[0], self.win_size[1])
+
+	#Internal use only methods
+	def __load_lights(self):
+		#load informations from file
+		cl_file = open(conf.illumination, "r")
+
+		print("from file:" + conf.illumination)
+		print("\tloaded lights settings...")
+
+		lights = []
+
+		line = cl_file.readline()
+		while line != "":
+			#loading light position (world coordinate)
+			n1 = line.find(" ")
+			x = float(line[:n1])
+			n2 = n1 + line[n1+1:].find(" ") + 1
+			pos = Point(x, float(line[n1:n2]), float(line[n2+1:]))
+
+			#ka loading
+			ka = float(cl_file.readline())
+
+			#Ia loading
+			line = cl_file.readline()
+			n1 = line.find(" ")
+			x = float(line[:n1])
+			n2 = n1 + line[n1+1:].find(" ") + 1
+			Ia = Vector(x, float(line[n1:n2]), float(line[n2+1:]))
+
+			#kd loading
+			kd = float(cl_file.readline())
+
+			#Od loading
+			line = cl_file.readline()
+			n1 = line.find(" ")
+			x = float(line[:n1])
+			n2 = n1 + line[n1+1:].find(" ") + 1
+			Od = Vector(x, float(line[n1:n2]), float(line[n2+1:]))
+
+			#ks loading
+			ks = float(cl_file.readline())
+
+			#Il loading
+			line = cl_file.readline()
+			n1 = line.find(" ")
+			x = float(line[:n1])
+			n2 = n1 + line[n1+1:].find(" ") + 1
+			Il = Vector(x, float(line[n1:n2]), float(line[n2+1:]))
+
+			#n loading
+			n = float(cl_file.readline())
+
+			lights.append(Light(pos, ka, Ia, kd, Od, ks, Il, n))
+			line = cl_file.readline()
+
+		print(str(len(lights)) + " light(s) loaded\n")
+		cl_file.close()
+
+		return lights
+
+	#debugging methods
+	def print(self):
+		pass
